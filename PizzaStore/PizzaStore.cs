bbd6ed109246fd6d5b52445e80dab12b3ed0de4c
Oliver Bresson <olbre20@student.sdu.dk>
2022-02-25 13:24:51 +0100

    test

--- PizzaStore/PizzaStore.cs
+++ PizzaStore/PizzaStore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PizzaStore
 {
@@ -14,13 +15,35 @@ namespace PizzaStore
                 Console.WriteLine("---- Pizza Store ----");
                 Console.WriteLine("'Order pizza' 'Exit'");
                 Console.Write("What would you like to do? ");
+
                 switch (Console.ReadLine())
                 {
                     case "Order pizza":
+
+                        Console.Write("First name: ");
                         string firstName = Console.ReadLine();
+
+                        Console.Write("Last name: ");
                         string lastName = Console.ReadLine();
+
+                        Console.Write("Mail: ");
                         string mail = Console.ReadLine();
-                        
+
+                        bool correctNum = false;
+                        int phoneNum;
+                        while (!correctNum)
+                        {
+                            Console.Write("Phone number: ");
+
+                            while (!int.TryParse(Console.ReadLine(), out phoneNum))
+                            {
+                                Console.WriteLine("Invalid phone number!");
+                                Console.WriteLine("Phone number should only contain numbers");                                
+                                Console.Write("Phone number: ");
+                            }
+
+                            correctNum = true;
+                        }
                         break;
                     case "Exit":
                         _exit = true;
